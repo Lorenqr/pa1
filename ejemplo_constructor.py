3b99587c9dcacf2b058dfb7ed0fcbac4e9f11561
lorenqr <lorenagr223@gmail.com>
2025-03-19 21:02:04 -0500

    Cambio de color

--- ejemplo_constructor.py
+++ ejemplo_constructor.py
@@ -6,7 +6,7 @@ from camion import Camion
 c1 = Coche("Ford", "Fiesta", "Rojo") # argumentos, en el llamado
 c2 = CocheDeportivo("Ford", "Fiesta", "Rojo", "GT")
 
-c3 = Camion("Kenworth", "T680", "Azul", 40)
+c3 = Camion("Kenworth", "T680", "Negro", 40)
 c3.adicionar_carga(25)
 
 print(c3)
